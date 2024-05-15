27ad68dd9f28b008b0c86ea8881f0c344c45dffa
kuniyan <57828407+kuni-0930@users.noreply.github.com>
2024-05-16 00:36:45 +0900

    delete files

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "astro/config";
-
-// https://astro.build/config
-export default defineConfig({
-  site: "https://kuni-0930.github.io",
-  //outDir: "./docs",
-  //base: "/kuni-0930.github.io",
-  //base: "/my-repo",
-});
